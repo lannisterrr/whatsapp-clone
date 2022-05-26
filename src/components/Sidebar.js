d76a7fbd264fa766a09d4eb535a5a5f309c13356
siddhant <siddhantp736@gmail.com>
2022-05-26 16:32:32 +0530

    [feat]: add a room to firestore

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,50 @@
+import { useState } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
-import { Add, ExitToApp, SearchOutlined } from '@material-ui/icons';
-import { auth } from '../firebase';
+import {
+  Add,
+  ExitToApp,
+  Message,
+  PeopleAlt,
+  SearchOutlined,
+} from '@material-ui/icons';
+import { auth, createTimeStamp, db } from '../firebase';
+import { NavLink } from 'react-router-dom';
+import { Home } from '@material-ui/icons';
 import './Sidebar.css';
 import './SidebarList.css';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import SidebarList from './SidebarList';
 
 export default function Sidebar({ user, page }) {
+  const [menu, setMenu] = useState(1);
+
+  const handleCreateRoom = () => {
+    const roomName = prompt('What is your public room name ? ');
+    if (roomName.trim()) {
+      db.collection('rooms').add({
+        name: roomName,
+        timeStamp: createTimeStamp(),
+      });
+    }
+  };
+
   function handleSignOut() {
     auth.signOut();
+  }
+
+  let Nav;
+  if (page.isMobile) {
+    Nav = NavLink;
+  } else {
+    Nav = props => (
+      <div
+        className={`${props.activeClass ? 'sidebar__menu--selected' : ''} `}
+        onClick={props.onClick}
+      >
+        {props.children}
+      </div>
+    );
   }
   return (
     <div
@@ -36,9 +74,71 @@ export default function Sidebar({ user, page }) {
           />
         </form>
       </div>
+      <div className="sidebar__menu">
+        <Nav
+          to="/chats"
+          onClick={() => setMenu(1)}
+          activeClass={menu === 1}
+          activeClassName="sidebar__menu--selected"
+        >
+          <div className="sidebar__menu--home">
+            <Home />
+            <div className="sidebar__menu--line" />
+          </div>
+        </Nav>
+
+        <Nav
+          to="/rooms"
+          onClick={() => setMenu(2)}
+          activeClass={menu === 2}
+          activeClassName="sidebar__menu--selected"
+        >
+          <div className="sidebar__menu--rooms">
+            <Message />
+            <div className="sidebar__menu--line" />
+          </div>
+        </Nav>
+
+        <Nav
+          to="/users"
+          onClick={() => setMenu(3)}
+          activeClass={menu === 3}
+          activeClassName="sidebar__menu--selected"
+        >
+          <div className="sidebar__menu--users">
+            <PeopleAlt />
+            <div className="sidebar__menu--line" />
+          </div>
+        </Nav>
+      </div>
+
+      {page.isMobile ? (
+        <Switch>
+          <Route path="/chats">
+            <SidebarList />
+          </Route>
+          <Route path="/rooms">
+            <SidebarList />
+          </Route>
+          <Route path="/users">
+            <SidebarList />
+          </Route>
+          <Route path="/search">
+            <SidebarList />
+          </Route>
+        </Switch>
+      ) : menu === 1 ? (
+        <SidebarList />
+      ) : menu === 2 ? (
+        <SidebarList />
+      ) : menu === 3 ? (
+        <SidebarList />
+      ) : menu === 4 ? (
+        <SidebarList />
+      ) : null}
 
       <div className="sidebar__chat--addRoom">
-        <IconButton>
+        <IconButton onClick={handleCreateRoom}>
           <Add />
         </IconButton>
       </div>
