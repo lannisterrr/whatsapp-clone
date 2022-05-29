63bf1be6013b5ca6d6c3abfbf170f10e1782ad86
siddhant <siddhantp736@gmail.com>
2022-05-29 15:19:49 +0530

    [feat]: timer Start when user clicks audio button

--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -4,8 +4,9 @@ import {
   MicRounded,
   Send,
 } from '@material-ui/icons';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './ChatFooter.css';
+import recordAudio from './recordAudio';
 
 // for recording live audio two properties are necessary
 // navigator.mediaDevices and window.MediaRecorder
@@ -18,14 +19,52 @@ export default function ChatFooter({
   room,
   roomID,
   image,
-  setImage,
+  setAudioId,
 }) {
   const [isRecording, setRecording] = useState(false);
+  const [duration, setDuration] = useState('00:00');
+
+  const timerInterval = useRef();
+  const recordingEl = useRef();
+  const record = useRef();
   const inputRef = useRef();
 
-  function handleStartRecording(event) {
+  async function handleStartRecording(event) {
     event.preventDefault();
+    record.current = await recordAudio();
     inputRef.current.focus(); //clicking on the button should open input audio recorder
+
+    inputRef.current.style.width = 'calc(100% - 56px)'; // to show approve or disapprove button
+    setRecording(true);
+    setAudioId('');
+  }
+
+  function pad(value) {
+    return String(value).length < 2 ? `0${value}` : value;
+  }
+
+  useEffect(() => {
+    if (isRecording) {
+      recordingEl.current.style.opacity = '1';
+      startTimer();
+      record.current.start();
+    }
+  }, [isRecording]);
+
+  function startTimer() {
+    const start = Date.now();
+    timerInterval.current = setInterval(setTime, 100);
+
+    // we are doing this because setInterval is not reliable ,
+    // we are checking for perfect time every 10th of a second
+    function setTime() {
+      const timeElapsed = Date.now() - start; // use of clousure , so we always get the start value
+      const totalSeconds = Math.floor(timeElapsed / 1000);
+      const minutes = pad(parseInt(totalSeconds / 60));
+      const seconds = pad(parseInt(totalSeconds % 60)); // 0-59
+      const duration = `${minutes}:${seconds}`;
+      setDuration(duration);
+    }
   }
 
   const btnIcons = (
@@ -88,7 +127,7 @@ export default function ChatFooter({
         )}
       </form>
       {isRecording && (
-        <div className="record">
+        <div ref={recordingEl} className="record">
           <CancelRounded
             style={{
               width: 30,
@@ -98,7 +137,7 @@ export default function ChatFooter({
           />
           <div>
             <div className="record__redcircle"></div>
-            <div className="record__duration"></div>
+            <div className="record__duration">{duration}</div>
           </div>
 
           <CheckCircleRounded
